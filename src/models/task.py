000cd63619ea7a0f7f017c4edc2a862e17a3c868
semayagi <yagid.semen07.07@yandex.ru>
2026-03-20 13:38:30 +0300

    Began transforming Task for safe publci API

--- src/models/task.py
+++ src/models/task.py
@@ -1,11 +1,34 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Protocol, Any
+import datetime
 
 @dataclass(slots=True) # slots=True reduces memory usage because, instead of each class object having a __dict__ that allows defining new attributes, there is a single immutable set of attributes defined in __slots__
-class Task:
+class Task(Protocol):
     '''
     Task model
     '''
 
-    id: int
-    payload: Any
+    id: str
+    payload: dict[str, Any]
+    
+    def execute(self, *args, **kwargs) -> Any:
+        ...
+        
+    @property
+    def task_type(self):
+        ...
+        
+    @property
+    def deadline(self) -> datetime.date | None:
+        ...
+    
+    
+class FileTask(Task):
+    id: str
+    payload: dict[str, Any]
+    
+    @property
+    def deadline(self):
+        return self.payload["deadline"]
+    
+    
